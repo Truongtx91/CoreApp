a9117a3d4a6687be522934a987e061c248635260
truongtx91 <truongtx91@gmail.com>
2019-08-27 07:24:36 +0700

    update indent

--- CoreApp.Web/Startup.cs
+++ CoreApp.Web/Startup.cs
@@ -23,6 +23,7 @@ namespace CoreApp.Web
         {
             // add some features
             services.AddMvc();
+            // change index
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
