f371ced5f12c490833195fd7cfb011f993074a3e
truongtx91 <truongtx91@gmail.com>
2019-08-27 07:46:25 +0700

    update index2

--- CoreApp.Web/Startup.cs
+++ CoreApp.Web/Startup.cs
@@ -24,6 +24,7 @@ namespace CoreApp.Web
             // add some features
             services.AddMvc();
             // change index
+            // change index2
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
